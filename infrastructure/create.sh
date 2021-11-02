f7f95c2298a8c1a19cfe909dd37a11d9b280a7a1
Lorenzo Ginyard <lnginyard@gmail.kenzie.academy.com>
2021-11-01 21:05:18 -0400

    edit README.md for repositroy

---
+++ infrastructure/create.sh
@@ -0,0 +1,1 @@
+aws cloudformation create-stack --stack-name challenge1 --template-body file://challenge1.yml  --parameters file://challenge1-parameters.js --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
